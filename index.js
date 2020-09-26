852a2f790550f1608e23958283fa9f61ddae8a54
Sumayya Syeda <sumayyasyeda@Admins-MacBook-Pro.local>
2020-09-26 11:37:30 -0700

    Shuttle API demo

---
+++ index.js
@@ -0,0 +1,27 @@
+//scripts comment for package.json: 
+//no"test": "echo \"Error: no test specified\" && exit 1"
+
+const express = require("express")
+const axios = require("axios")
+
+const app = express()
+
+app.get("/",  function (req, res){
+    res.send("Shuttle API");
+});
+
+app.get("/locations", function (req, res){
+    axios
+        .get(
+            "http://andysbuses.com/Services/JSONPRelay.svc/GetMapVehiclePoints?ApiKey=8882812681"
+        )
+        .then(function (data) { //function (data) same thing as (data) =>
+            res.send(data.data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send("Error");
+        });
+});
+
+app.listen(5000, () => console.log('App Listening on port 5000'));
